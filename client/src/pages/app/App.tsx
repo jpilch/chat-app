5cac5cdfcfdab709d13bcba69a3623c970d935af
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-08-04 18:36:32 +0200

    initialize socket connection from client

--- client/src/pages/app/App.tsx
+++ client/src/pages/app/App.tsx
@@ -1,9 +1,13 @@
 import styles from "./App.module.css"
-console.log(styles)
+
+import { useSocket } from "../../hooks"
 
 function App() {
+    const socket = useSocket();
+
     return (
         <div className="app">
+            <p>test</p>
         </div>
     )
 }
