88ddd3de169aa728fc47fa88a0fea2998d1311ed
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-08-04 18:07:59 +0200

    get env variables from process.env

--- server/env.ts
+++ server/env.ts
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = 3000;
+export const PORT = process.env.PORT;
 
-export const CLIENT_URL = "http://localhost:5173";
+export const CLIENT_URL = process.env.CLIENT_URL;
