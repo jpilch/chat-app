657cdcd8143d8892c04faae2713a0a5383ddea59
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-08-05 14:49:10 +0200

    group files by feature

--- client/src/pages/app/app.tsx
+++ client/src/pages/app/app.tsx
@@ -1,10 +1,6 @@
 import styles from "./App.module.css"
 
-import { useSocket } from "../../hooks"
-
 function App() {
-    const socket = useSocket();
-
     return (
         <div className="app">
             <p>test</p>
