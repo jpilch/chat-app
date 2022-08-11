c5e3b24f2d17c4142ee0642f1da43e02ff4754a3
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-08-11 15:27:42 +0200

    work on signup page

--- client/src/features/auth/components/SignUpForm.tsx
+++ client/src/features/auth/components/SignUpForm.tsx
@@ -1,8 +1,41 @@
-import React from "react";
+import './SignUpForm.scss';
 
 function SignUpForm() {
     return (
-        <div>SignUpForm</div>
+        <div className='sign-up-form'>
+            <form className="sign-up-form__form">
+                <input
+                    type="text"
+                    className="sign-up-form__input"
+                    id='email'
+                    placeholder='Email address'
+                />
+                <input
+                    type="text"
+                    className="sign-up-form__input"
+                    id='username'
+                    placeholder='Username'
+                />
+                <input
+                    type="password"
+                    className="sign-up-form__input"
+                    id='password'
+                    placeholder='Password'
+                />
+                <input
+                    type="password"
+                    className="sign-up-form__input"
+                    id='password-confirm'
+                    placeholder='Repeat password'
+                />
+                <button
+                    className='sign-up-form__button'
+                    type='submit'
+                >
+                    Sign Up
+                </button>
+            </form>
+        </div>
     );
 }
 
