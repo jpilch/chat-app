bc10d7c5cc7688159d9f65a5f9309930843aa076
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-08-11 17:59:29 +0200

    replace scss with css modules and css variables

--- client/src/features/app/components/CompanyTrademark.tsx
+++ client/src/features/app/components/CompanyTrademark.tsx
@@ -1,13 +1,13 @@
-import "./CompanyTrademark.scss";
+import styles from './CompanyTrademark.module.css'
 
 function CompanyTrademark() {
     return (
-        <div className="trademark">
-            <div className="trademark__logo">
-                <h1 className='trademark__name'>Comms</h1>
-                <img src="/src/assets/logo-500x500.png" alt="" className="trademark__img" />
+        <div className={styles.trademark}>
+            <div className={styles.trademark__logo}>
+                <h1 className={styles.trademark__name}>Comms</h1>
+                <img src="/src/assets/logo-500x500.png" alt="company logo" className={styles.trademark__img} />
             </div>
-            <p className='trademark__heading'>Real time chatting has never been easier</p>
+            <p className={styles.trademark__heading}>Real time chatting has never been easier</p>
         </div>
     );
 }
