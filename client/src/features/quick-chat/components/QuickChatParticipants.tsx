1a6ce9a20416c569b036ec3b0636de0184e187e0
Jan Pilch <pilch.jan.kontakt@gmail.com>
2022-08-23 10:44:28 +0200

    add filter functionality to participants

--- client/src/features/quick-chat/components/QuickChatParticipants.tsx
+++ client/src/features/quick-chat/components/QuickChatParticipants.tsx
@@ -7,11 +7,13 @@ import { selectParticipants } from "../state/quickChatSlice";
 
 import uniqueId from "lodash/uniqueId";
 import { selectUsername } from "../../auth/authSlice";
+import { useState } from "react";
 
 function QuickChatParticipants() {
 
     const participants = useAppSelector(selectParticipants);
     const username = useAppSelector(selectUsername);
+    const [filter, setFilter] = useState<string>("");
 
     return (
         <div className={styles.sidebar}>
@@ -20,16 +22,23 @@ function QuickChatParticipants() {
                 type="text"
                 className={styles.sidebar__search}
                 placeholder="Search"
+                onChange={(e) => setFilter(e.target.value.toLocaleLowerCase())}
             />
             <div className={styles.sidebar__entries}>
-                {participants.map(participant => (
-                    <QuickChatParticipant
-                        username={participant.username}
-                        isTyping={participant.isTyping}
-                        isCurrent={username === participant.username}
-                        key={uniqueId(participant.username)}
-                    />
-                ))}
+                {participants
+                    .filter(p => {
+                        return filter
+                            ? p.username.toLocaleLowerCase().includes(filter)
+                            : true;
+                    })
+                    .map(participant => (
+                        <QuickChatParticipant
+                            username={participant.username}
+                            isTyping={participant.isTyping}
+                            isCurrent={username === participant.username}
+                            key={uniqueId(participant.username)}
+                        />
+                    ))}
             </div>
         </div>
     );
